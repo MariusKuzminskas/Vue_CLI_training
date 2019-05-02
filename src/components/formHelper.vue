6de1965bf98a4ce5fa0d6443d4a1f72382557c43
marius <marius@scoding.com>
2019-05-02 18:01:11 +0300

    #28 start Dynamic components

--- src/components/formHelper.vue
+++ src/components/formHelper.vue
@@ -1,48 +1,39 @@
 <template>
-  <footer>
+    <div>
     <h1>Please fill out our form...</h1>
-    <form action="">
-      <div id="form-header">
-        <slot name="form-header"></slot>
-      </div>
-      <div id="form-fields">
-        <slot name="form-fields"></slot>
-      </div>
-      <div id="form-control">
-        <slot name="form-control"></slot>
-      </div>
-      <div id="useful-links">
-        <ul>
-          <li><a href="">Link_1</a></li>
-          <li><a href="">Link_2</a></li>
-          <li><a href="">Link_3</a></li>
-          <li><a href="">Link_4</a></li>
-          <li><a href="">Link_5</a></li>
-
-              
-        </ul>
-      </div>
-
-
-    </form>
-  </footer>
+        <form>
+            <div id="form-header">
+                <slot name="form-header"></slot>
+            </div>
+            <div id="form-fields">
+                <slot name="form-fields"></slot>
+            </div>
+            <div id="form-controls">
+                <slot name="form-controls"></slot>
+            </div>
+            <div id="useful-links">
+                <ul>
+                    <li><a href="#">link 1</a></li>
+                    <li><a href="#">link 2</a></li>
+                    <li><a href="#">link 3</a></li>
+                    <li><a href="#">link 4</a></li>
+                </ul>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-
-
+// Imports
 export default {
-  components:{
-
-  },
-  data () {
-    return {
-      
+    components: {
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
     }
-  },
-  methods: {
-
-  }
 }
 </script>
 
