b7555104ad5d3760c483bf91174da84e002d31d2
marius <marius@scoding.com>
2019-05-03 12:25:45 +0300

    webpack vunerability

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
     <div>
-        <keep-alive>
-        <component v-bind:is="component" ></component>
-        </keep-alive>
-        <button v-on:click="component = 'form-one'" >Show form one</button>
-        <button v-on:click="component = 'form-two'" >Show form two</button>
+        
     </div>
 </template>
 
 <script>
-// Imports
-import formOne from './components/formOne.vue';
-import formTwo from './components/formTwo.vue';
+
 export default {
-    components: {
-        'form-one': formOne,
-        'form-two': formTwo
-    },
+    
     data () {
         return {
-            component: 'form-two'
+            
         }
     },
     methods: {
-        handleSubmit: function(){
-            alert('thanks for submitting');
-        }
+        
     }
 }
 </script>
